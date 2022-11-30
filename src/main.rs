d9f172fa9452d69b7b01e18159c2f95e59f3a6f4
Jasper Fedora VM <jasper.vandenhoven@fox-it.com>
2022-11-30 23:16:09 +0100

    Added initial case handling argument parsing logic to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io,env, process::exit};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        print!("Usage: rust-text-converter [argument] [input string]");
+        exit(1)
+    } 
+    else {
+        println!("Rust Text Converter\n");
+        let set_case = &args[1];
+        let src_test = &args[2];
+    
+        match set_case.as_str() {
+            "-c" | "--caps" => println!("Printing to UPPER CASE"),
+            "-l" | "--lower" => println!("Printing to lower case"),
+            "-a" | "--alt" => println!("Printing to AlTeRnAtiVe CaSe"),
+            "-i" | "--invalt" => println!("Printing to iNvErTeD aLtErNaTiVe cAsE"),
+            "-r" | "--random" => println!("Printing to ranDOM CAsE"),
+            "-s" | "--leet" => println!("Printing to l337$p34k"),
+            "-g" | "--angry" => println!("Printing to Angry. Case."),
+            "-t" | "--trueangry" => println!("Printing to REALLY. ANGRY. CASE."),
+            _ => println!("Invalid case type: {}", set_case)
+        }
+    
+        println!("Case setting: {}", set_case);
+        println!("Text to transform: {}", src_test);
+        Ok(())
+    }
 }
