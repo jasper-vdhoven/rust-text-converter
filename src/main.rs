1d3038fde856109d75e2c57d9a744adc0d9ea585
Jasper Fedora VM <contact@bollos.nl>
2022-12-01 22:06:31 +0100

    Added emoji handling functionality - initial functionality

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,45 @@ fn convert_to_leetspeak(input_string:&String) -> String {
     return converted_string;
 }
 
+fn convert_shortcodes_to_emojis(input_string:&String) -> Cow<str> {
+    // Takes a shortcode :smile: and converts it into the equivalent emoji
+    // Use node_emoji::Replacer to search through a string for all matches in one go
+    let replacer: Replacer = node_emoji::Replacer::new();
+    let converted_string: Cow<str> = replacer.replace_all(input_string);
+
+    return converted_string;
+}
+
+fn emoji_case(input_string:&String) -> Vec<String> {
+    let mut emoji_array: Vec<String> = vec![];
+
+    // takes a string and substitutes words -> emojis where possible
+    // dog -> dog emoji | ocean -> ocean emoji
+    for words in input_string.to_lowercase().split(" ").collect::<Vec<_>>() {
+        let emoji_char = node_emoji::get(words);
+        // If the entered emoji string is invalid, push the original value to the vec
+        if emoji_char.is_none() {
+            emoji_array.push(words.to_string());
+        }
+        // Else, pus the translated emoji equivalent to the vec
+        else {
+            emoji_array.push(emoji_char.unwrap().to_string());
+        }
+    }
+    return emoji_array;
+}
+
+fn decode_emojis_to_shortcode(input_string:&String) -> Option<Vec<String>> {
+    let mut emoji_arary: Vec<String> = vec![]; 
+    // Convert emoji input into short code, this is only for a single emoji (I think)
+    for emoji in input_string.graphemes(false) {
+        let emoji_char: &Emoji = emojis::get(emoji)?;
+        emoji_arary.push(format!(":{}:", emoji_char.name().to_string()));
+    }
+
+    return Some(emoji_arary);
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
